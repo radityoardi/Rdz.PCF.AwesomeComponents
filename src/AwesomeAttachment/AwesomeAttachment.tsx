439ade1e8238a252737604a5aff1b800c664384e
Radityo Ardi <radityo.ardi@insight.com>
2022-06-27 10:58:31 +0800

    all things

--- src/AwesomeAttachment/AwesomeAttachment.tsx
+++ src/AwesomeAttachment/AwesomeAttachment.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as Fluent from '@fluentui/react';
 import { IInputs, IOutputs } from "./generated/ManifestTypes";
-import Dropzone from 'react-dropzone';
+import Dropzone, { FileRejection } from 'react-dropzone';
 import { v4 as guid } from 'uuid';
 import * as FluentIcon from '@fluentui/react-file-type-icons';
 
@@ -24,7 +24,8 @@ export class AwesomeAttachment extends React.Component<IAwesomeAttachmentProps> 
   private App: React.FunctionComponent<IAwesomeAttachmentProps> = (props: IAwesomeAttachmentProps) => {
     const [files, setFiles] = React.useState<IFile[]>([]);
     const [deletedFiles, setDeletedFiles] = React.useState<IFile[]>([]);
-    const csvPreviewExtensions = `jpg,jpeg,png,bmp`;
+    const [maxFiles, setMaxFiles] = React.useState<number>(props.context?.parameters.maxFileCount.raw!);
+    const [csvPreviewExtensions, setCsvPreviewExtensions] = React.useState<string>(props.context?.parameters.extensionsPreview.raw!);
 
     const fileExtension = (filename: string) => {
       const lastIndexOfDot = filename.lastIndexOf(`.`);
@@ -43,6 +44,13 @@ export class AwesomeAttachment extends React.Component<IAwesomeAttachmentProps> 
       setFiles(splicedFiles);
     };
 
+    const deleteAllFiles = () => {
+      files.map(file => {
+        URL.revokeObjectURL(file.preview);
+      });
+      setFiles([]);
+    };
+
     const renderImage = (currentFile: IFile, extension: string) => {
       if (csvPreviewExtensions.indexOf(extension) > -1) {
         return (<Fluent.Image height={64} src={currentFile.preview} imageFit={Fluent.ImageFit.centerCover} />);
@@ -51,11 +59,19 @@ export class AwesomeAttachment extends React.Component<IAwesomeAttachmentProps> 
       }
     };
 
-    const handleMaxFiles = () => {
-      if (props.context && props.context.parameters.maxFileCount.raw! > 0) {
-        return props.context.parameters.maxFileCount.raw!;
+    const onDrop = (acceptedFiles:File[], rejectedFiles:FileRejection[]) => {
+      if (maxFiles === undefined || (maxFiles && files.length <= maxFiles)) {
+        const remainingSlots = maxFiles === undefined ? undefined : maxFiles - files.length;
+        const finalAcceptedFiles = remainingSlots === undefined ? acceptedFiles : acceptedFiles.slice(0, remainingSlots);
+        console.log(finalAcceptedFiles);
+        setFiles(current => [...current, ...finalAcceptedFiles.map((acceptedFile:File) => {
+          return {
+            ID: guid(),
+            properties: acceptedFile,
+            preview: URL.createObjectURL(acceptedFile)
+          } as IFile;
+        })]);
       }
-      return undefined;
     };
 
     React.useEffect(() => {
@@ -66,44 +82,52 @@ export class AwesomeAttachment extends React.Component<IAwesomeAttachmentProps> 
       }
     }, []);
 
+    React.useEffect(() => {
+      setMaxFiles(props.context?.parameters.maxFileCount.raw!);
+    },[props.context?.parameters.maxFileCount.raw!]);
+
+    React.useEffect(() => {
+      setCsvPreviewExtensions(props.context?.parameters.extensionsPreview.raw!);
+    },[props.context?.parameters.extensionsPreview.raw!]);
+
     return (
       <React.Fragment>
         <Fluent.Stack tokens={{ childrenGap: 10 }}>
-          <Dropzone maxFiles={handleMaxFiles()} onDrop={(acceptedFiles) => {
-            setFiles(current => [...current, ...acceptedFiles.map((acceptedFile) => {
-              return {
-                ID: guid(),
-                properties: acceptedFile,
-                preview: URL.createObjectURL(acceptedFile)
-              } as IFile;
-            })]);
-          }}>
+          <Dropzone maxFiles={maxFiles > 0 ? maxFiles : undefined} onDrop={onDrop.bind(this)}>
             {
               ({ getRootProps, getInputProps }) => (
                 <section>
                   <div {...getRootProps()} className={`awesomeattachment-dropzone`}>
-                    <input {...getInputProps()} />
-                    <Fluent.Text variant='mediumPlus' nowrap>
-                      {props.context && props.context.parameters.dragDropPlaceholderTexts.raw!}
-                    </Fluent.Text>
-
+                    <Fluent.Stack>
+                      <input {...getInputProps()} />
+                      <Fluent.Text variant='mediumPlus' nowrap>
+                        {props.context && props.context.parameters.dragDropPlaceholderTexts.raw!}
+                      </Fluent.Text>
+                      {
+                        maxFiles > 0 && (<Fluent.Text variant='smallPlus' nowrap>(up to {maxFiles} file{maxFiles > 1 ? `s` : ``})</Fluent.Text>)
+                      }
+                    </Fluent.Stack>
                   </div>
                 </section>
               )
             }
           </Dropzone>
+          <Fluent.Stack horizontal>
+            <Fluent.ActionButton iconProps={ { iconName: "Delete" } } text="Clear everything" onClick={deleteAllFiles.bind(this)} disabled={ files.length === 0 } />
+          </Fluent.Stack>
           <Fluent.Stack tokens={{ childrenGap: 10 }} className={`awesomeattachment-dropzone-files`} horizontal wrap>
             {
               (files === undefined || files.length === 0) && props.context && (
-                <Fluent.Text variant='small' nowrap key={`nofiletext-${guid()}`}>
-                  {props.context.parameters.noFileTexts.raw!}
-                </Fluent.Text>
+                <Fluent.Stack tokens={ { padding: 20 } }>
+                  <Fluent.Text variant='small' nowrap key={`nofiletext-${guid()}`}>
+                    { props.context.parameters.noFileTexts.raw! }
+                  </Fluent.Text>
+                </Fluent.Stack>
               )
             }
             {
               files && files.length > 0 && files.map((file, index) => {
                 const extension = fileExtension(file.properties.name);
-                console.log(file);
                 return (
                   <Fluent.StackItem tokens={{ padding: 10 }} key={`file-${file.ID}`} className={`awesomeattachment-dropzone-file`}>
                     {
